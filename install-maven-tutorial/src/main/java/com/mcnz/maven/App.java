17b5cf3a9692ec09041bcfbc814ddedbfa4b191d
sourabh <sourabhphanda@gmail.com>
2019-06-15 13:14:53 +0530

    maven project

---
+++ install-maven-tutorial/src/main/java/com/mcnz/maven/App.java
@@ -0,0 +1,13 @@
+package com.mcnz.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
